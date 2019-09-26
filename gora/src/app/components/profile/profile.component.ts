137db40b69f2f6ee8689d8af2c14cb952e7395cc
Aleksandr_Pletenev <Aleksandr_Pletenev@epam.com>
2019-09-26 18:55:43 +0400

    add profile form

--- gora/src/app/components/profile/profile.component.ts
+++ gora/src/app/components/profile/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Interest } from 'src/app/interfaces/interest';
+import { MyTabsEvent } from 'src/app/interfaces/my-tabs-event';
+import { FriendsList } from 'src/app/interfaces/friends-list';
 
 @Component({
   selector: 'app-profile',
@@ -18,14 +20,36 @@ export class ProfileComponent implements OnInit {
       name: 'Компьютеры'
     },
     {
-      id:3,
-      name:'Радио'
+      id: 3,
+      name: 'Радио'
     }
-  ]
+  ];
 
+  public profile: FriendsList = {
+    id: 1,
+    img: '/assets/image/gora.jpg',
+    name: 'Виталя Гора',
+    city: 'г. Нижние Шахты',
+    phone: '+7 (440) 554-32-12',
+    email: 'vitalya@gora.ru',
+    interest: this.interests
+  };
+
+  public write: boolean = false;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public deleteInterest(id: number): void {
+    let indexDelet: number = this.interests.findIndex(
+      itemInterest => itemInterest.id === id
+    );
+    this.interests.splice(indexDelet, 1);
+  }
+
+  public clickInterest(event: MyTabsEvent) {
+    let dataLink = event.target.getAttribute('data-link');
   }
 
 }
